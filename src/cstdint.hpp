283a4a5e61697bb593678f89313a5d506b85506b
noctor <129334069+noctorr@users.noreply.github.com>
2026-07-02 00:43:06 +0100

    Add cstdint.hpp for fixed-width integer types

---
+++ src/cstdint.hpp
@@ -0,0 +1,36 @@
+#include "type_traits.hpp"
+
+constexpr int INT_SIZE { static_cast<int>(sizeof(int)) * 8 };
+
+consteval bool Check ()
+{
+    if constexpr ( INT_SIZE == 32 ) 
+        return true;
+    else
+        return false;
+}
+
+#ifndef CSTDINT_HPP
+#define CSTDINT_HPP
+
+namespace std {
+using fast_uint32 = unsigned int;
+using fast_uint64 = unsigned long long;
+using fast_int8 = char;
+using fast_int16 = short;
+
+using uint8 = unsigned char;
+using uint16 = unsigned short;
+using uint64 = unsigned long long;
+
+using uint32 = typename decltype([]{
+    if constexpr (Check()) 
+        return std::TYPE_IDENTITY<unsigned int>{ };
+    else
+        return std::TYPE_IDENTITY<unsigned long>{ };
+}())::selfType;
+
+constexpr uint32 UINT32_MAX { 4294967278 };
+}
+
+#endif
